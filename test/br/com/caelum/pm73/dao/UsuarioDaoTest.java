b7ac7133941a8efc36a455f0b4229244e3dd6ca4
flavio.santana <9090flavio@gmail.com>
2018-09-20 21:43:54 -0300

    Criando projeto

--- test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
+++ test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
@@ -1,4 +1,28 @@
 package br.com.caelum.pm73.dao;
 
+import br.com.caelum.pm73.dominio.Usuario;
+import org.hibernate.Session;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UsuarioDaoTest {
+
+    public static final String NOME_JOÃO_DA_SILVA = "João da Silva";
+    public static final String EMAIL_JOAO = "a@b.c";
+
+    @Test
+    public void buscarPorNomeEEmail(){
+
+        Session session = new CriadorDeSessao().getSession();
+        UsuarioDao usuarioDao = new UsuarioDao(session);
+
+        Usuario usuario = usuarioDao.porNomeEEmail(NOME_JOÃO_DA_SILVA, EMAIL_JOAO);
+
+        Assert.assertEquals(usuario.getNome(), "");
+        Assert.assertEquals(usuario.getEmail(), "");
+
+
+
+    }
+
 }
